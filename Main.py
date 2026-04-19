b8d575af2cc102d7f5ce163c86794b774897dc7a
loveliedestine24-art <loveliedestine24@gmail.com>
2026-04-19 03:35:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from threading import Thread
 
-# Configuração para o Render não dar erro de "Port"
+# Konfigirasyon pou Render
 server = Flask(__name__)
 
 @server.route("/")
@@ -11,11 +11,10 @@ def webhook():
     return "Lolybot ativo!", 200
 
 def run():
-    # O Render usa a porta 10000 por padrão no plano gratuito
     server.run(host="0.0.0.0", port=10000)
 
-# O teu Token do Telegram
-TOKEN = "8227900082:"AAGqkCKieoIvtdpKFH4czXpDnwwPRxz8vIc"
+# Nouvo Token ou a
+TOKEN = "8227900082:AAGqkCKieoIvtdpkFH4czXpDnwwPRxz8vIc"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -23,8 +22,7 @@ def send_welcome(message):
     bot.reply_to(message, "Bot la ap mache sou GitHub/Render kounye a! 🚀")
 
 if __name__ == "__main__":
-    # Inicia o servidor Flask numa thread separada
     Thread(target=run).start()
-    
     print("Bot a iniciar...")
     bot.infinity_polling()
+
